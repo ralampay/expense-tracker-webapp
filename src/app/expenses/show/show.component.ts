5eea64d2854881504a0c5e2854bfa73e09c9219c
Raphael Alampay <raphael.alampay@gmail.com>
2023-01-20 14:20:07 +0800

    added show component and get id value from url path

--- src/app/expenses/show/show.component.ts
+++ src/app/expenses/show/show.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-expense-item-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.scss']
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit {
+
+  id : number
+
+  constructor(private route : ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'))
+    console.log(`ShowComponent for id ${this.id}`)
+  }
 
 }
