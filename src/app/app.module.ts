51e3e6eaece8f7a165e8fefd835c54791ffabe07
Raphael Alampay <raphael.alampay@gmail.com>
2023-01-20 13:40:24 +0800

    added routes for static content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ExpenseItemComponent } from './expenses/item/expense-item.component';
@@ -9,6 +10,17 @@ import { FooterComponent } from './sections/footer/footer.component';
 import { ExpenseTrackerFormComponent } from './expenses/form/expense-tracker-form.component';
 import { FormsModule } from '@angular/forms';
 import { CategorySelectComponent } from './expenses/category-select/category-select.component';
+import { IndexComponent } from './components/index/index.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
+const appRoutes : Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact-us', component: ContactUsComponent }
+]
+
+
 
 @NgModule({
   declarations: [
@@ -17,12 +29,16 @@ import { CategorySelectComponent } from './expenses/category-select/category-sel
     HeaderComponent,
     FooterComponent,
     ExpenseTrackerFormComponent,
-    CategorySelectComponent
+    CategorySelectComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ExpenseItem } from '../../models/expense-item';
+import {ExpenseCalculatorService} from '../../services/expense-calculator.service';
+import {ExpenseItemsService} from '../../services/expense-items.service';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent implements OnInit {
+
+  title: string = "Expense Tracker";
+  description: string = "An expense tracker for personal use";
+
+  expenseItems: ExpenseItem[] = [];
+
+  categoryId : number
+
+  totalExpenses : number = 0.00
+  averageExpenses : number = 0.00
+
+  constructor(
+    private expenseCalculatorService : ExpenseCalculatorService, 
+    private expenseItemsService : ExpenseItemsService
+  ) {}
+
+  ngOnInit() : void {
+    console.log("ngOnInit() fired for AppComponent")
+
+    this.expenseItemsService.getAll().subscribe((expenseItems) => {
+      this.expenseItems = expenseItems
+    })
+  }
+
+  // Handler to update categoryId from category-select component event
+  setCategoryId = (payload: any) => {
+    this.categoryId = payload.id
+  }
+
+  formEventHandler = (payload: ExpenseItem) => {
+    console.log("Handling formEventHandler...")
+    console.log(payload)
+
+    // Insert currently maintained categoryId to the payload before adding to list
+    payload.category_id = this.categoryId
+
+    this.expenseItems.push(payload)
+    this.totalExpenses = this.expenseCalculatorService.computeAllExpenses(this.expenseItems)
+    this.averageExpenses = this.expenseCalculatorService.computeAverageExpenses(this.expenseItems)
+  }
+}
